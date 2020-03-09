90dc38a296ecc4d34a6b40bfb304c19c502d48db
Paulo Eduardo <55207594+Paulo-Ed@users.noreply.github.com>
2020-03-09 10:04:26 -0300

    Add files via upload

---
+++ Adivinhacao.py
@@ -0,0 +1,13 @@
+numero = 24
+chute = int(input("Digite um numero\n"))
+#if numero == chute:
+   # print("Voce acertou!!!")
+#else:
+    #print("Você errou!!!")
+
+if	(numero	==	chute):
+		print('Você	acertou!')
+elif	(chute	>	numero):
+		print('Você	errou!	O	seu	chute	foi	maior	que	o	número	secreto')
+elif	(chute	<	numero):
+		print('Você	errou!	O	seu	chute	foi	menor	que	o	número	secreto')
